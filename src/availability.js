6b3e9763dfcd748c815408dfcd3faac0497f9313
Chinosu <46995931+Chinosu@users.noreply.github.com>
2023-12-09 23:49:29 +1100

    Add Availability comparator

--- src/availability.js
+++ src/availability.js
@@ -1,6 +1,12 @@
 export default class Availability {
-  constructor(day, start) {
+  constructor(day, time) {
     this.day = day;
-    this.start = start;
+    this.time = time;
+  }
+
+  static same(availabilityOne, availabilityTwo) {
+    const { dayOne, timeOne } = availabilityOne;
+    const { dayTwo, timeTwo } = availabilityTwo;
+    return dayOne === dayTwo && timeOne === timeTwo;
   }
 }
