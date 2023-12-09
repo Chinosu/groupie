efe3e33660a432a8c507e99a493e4c5805eab237
Chinosu <46995931+Chinosu@users.noreply.github.com>
2023-12-09 23:29:30 +1100

    Fix self. -> this.

--- availability.js
+++ availability.js
@@ -1,7 +1,7 @@
 export default class Availability {
   constructor(day, start, end) {
-    self.day = day;
-    self.start = start;
-    self.end = end;
+    this.day = day;
+    this.start = start;
+    this.end = end;
   }
 }
