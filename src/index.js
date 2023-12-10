d9c4356d298eda6e906b133bbe7b13cd4697b771
Chinosu <46995931+Chinosu@users.noreply.github.com>
2023-12-10 13:09:23 +1100

    Add basic AvailabilityManager test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import AvailabilityManager from "./availability-manager.js";
-import Availability from "./availability.js";
-import Person from "./person.js";
 
 console.log('hello, JavaScript.');
 
-const people = [];
-people.push(new Person('Bob', new Availability(1, 2), new Availability(2, 3)));
-people.push(new Person('Chris', new Availability(1, 2), new Availability(1, 3)));
-const manager = new AvailabilityManager(people);
-console.log(manager.calculateGroupAvailability());
+const manager = new AvailabilityManager(1, 3);
+manager.updatePerson('joe', [AvailabilityManager.timeslot(1, 2)]);
+manager.updatePerson('paul', [AvailabilityManager.timeslot(1, 2), AvailabilityManager.timeslot(1, 3)]);
+manager.updatePerson('bob');
+manager.updatePerson('chris', [AvailabilityManager.timeslot(1, 1), AvailabilityManager.timeslot(1, 2), AvailabilityManager.timeslot(1, 3), AvailabilityManager.timeslot(1, 4)]);
+manager.print();
