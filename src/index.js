1e9a07796aea5236678baf037859be94f177a3b8
Chinosu <46995931+Chinosu@users.noreply.github.com>
2023-12-10 13:19:17 +1100

    Add brief peoples test

--- src/index.js
+++ src/index.js
@@ -8,3 +8,4 @@ manager.updatePerson('paul', [AvailabilityManager.timeslot(1, 2), AvailabilityMa
 manager.updatePerson('bob');
 manager.updatePerson('chris', [AvailabilityManager.timeslot(1, 1), AvailabilityManager.timeslot(1, 2), AvailabilityManager.timeslot(1, 3), AvailabilityManager.timeslot(1, 4)]);
 manager.print();
+console.log('People:', manager.people());
