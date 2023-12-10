36fe42fbc83d6a543626c55ae187d46b14f3f55d
Chinosu <46995931+Chinosu@users.noreply.github.com>
2023-12-10 12:38:16 +1100

    Rebuild class AvailabilityManager

--- src/availability-manager.js
+++ src/availability-manager.js
@@ -1,24 +1,16 @@
 export default class AvailabilityManager {
-  constructor(people) {
-    this.people = people ?? [];
-    this.people.flat();
+  static get numTimeSlotsPerDay() {
+    return 48;
   }
 
-  addPeople(people) {
-    this.people.concat(people);
-  }
+  constructor(startDay, numDays) {
+    this.startDay = startDay;
+    this.numDays = numDays;
 
-  calculateGroupAvailability() {
-    const availabilities = [];
-    this.people.flatMap(person => person.availabilities).forEach(availability => {
-      if (this.people.some(person => person.availabilities.every(a => !a.equals(availability)))) {
-        return;
-      }
-      if (availabilities.some(a => a.equals(availability))) {
-        return;
-      }
-      availabilities.push(availability);
+    this.timeslots = Array.from({ length: this.numDays }, () => {
+      return Array.from({ length: AvailabilityManager.numTimeSlotsPerDay }, (v, k) => {
+        return new Set();
+      });
     });
-    return availabilities;
   }
 }
