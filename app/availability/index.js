870fa0639684b4ed07f49c8d853fbca13bcc97d2
Chinosu <46995931+Chinosu@users.noreply.github.com>
2023-12-22 20:16:46 +1100

    Restore old files

---
+++ app/availability/index.js
@@ -0,0 +1,11 @@
+import AvailabilityManager from "./availability-manager.js";
+
+console.log('hello, JavaScript.');
+
+const manager = new AvailabilityManager(1, 3);
+manager.updatePerson('joe', [AvailabilityManager.timeslot(1, 2)]);
+manager.updatePerson('paul', [AvailabilityManager.timeslot(1, 2), AvailabilityManager.timeslot(1, 3)]);
+manager.updatePerson('bob');
+manager.updatePerson('chris', [AvailabilityManager.timeslot(1, 1), AvailabilityManager.timeslot(1, 2), AvailabilityManager.timeslot(1, 3), AvailabilityManager.timeslot(1, 4)]);
+manager.print();
+console.log('People:', manager.people());
